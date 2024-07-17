2abc80f6c2d62d3ce11b9cdf5898e4e7e305ac27
Sofya <s.cheboltasova@g.nsu.ru>
2024-07-18 03:04:50 +0700

    fix: add routing to Resources Page

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ import { pages } from "./assets/consts/pages";
 import Nav from "./components/Nav/Nav";
 import Main from "./components/Main/Main";
 import MapComponent from "./components/MapComponent/MapComponent";
-import SearchBar from "./components/SearchBar/SearchBar";
+// import SearchBar from "./components/SearchBar/SearchBar";
+import ResourcesPage from "./components/ResourcesPage/ResourcesPage";
 
 function App() {
   return (
@@ -24,7 +25,7 @@ function App() {
           />
           <Route
             path={pages.tickets.path}
-            element={<Main children={<SearchBar />} />}
+            element={<Main children={<ResourcesPage />} />}
           />
         </Routes>
       </section>
