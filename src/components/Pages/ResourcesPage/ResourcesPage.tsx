d36e32c6efa1d867f21e9330674b8f056859fe2f
Sofya <s.cheboltasova@g.nsu.ru>
2024-07-19 01:51:48 +0700

    feat: filter Modals by search input

--- src/components/Pages/ResourcesPage/ResourcesPage.tsx
+++ src/components/Pages/ResourcesPage/ResourcesPage.tsx
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from "react";
 
-import resources from "../../../assets/json/resources.json";
 import style from "./ResourcesPage.module.scss";
+import resources from "../../../assets/json/resources.json";
 
 import Modal from "../../Elements/Modal/Modal";
-import ChannelData from "../../../utils/interfaces/ChannelData";
 import TelegramApi from "../../api/TelegramApi";
+import SearchBar from "../../Elements/SearchBar/SearchBar";
+import Header from "../../Elements/Header/Header";
+import ModalProps from "../../../utils/interfaces/ModalProps";
 
-const ResourcesPage = React.memo(() => {
-  const [channelData, setChannelData] = useState<ChannelData[]>([]);
+export default function ResourcesPage() {
+  const [channelData, setChannelData] = useState<ModalProps[]>([]);
+  const [searchInput, setSearchInput] = useState<string>("");
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -22,7 +25,7 @@ const ResourcesPage = React.memo(() => {
         );
         setChannelData(allChannelData.flat());
       } catch (error) {
-        console.error("Error fetching channel data:", error);
+        console.error("Error fetching channel data");
       } finally {
         setIsLoading(false);
       }
@@ -33,22 +36,54 @@ const ResourcesPage = React.memo(() => {
     }
   }, []);
 
+  useEffect(() => {
+    const searchedData: ModalProps[] = channelData.map((data) => {
+      const isInputEmpty: boolean = searchInput.trim() === "";
+      if (isInputEmpty) return { ...data, hidden: false };
+
+      const { title, description, theme } = data;
+      const isInputMatch: boolean = isSubstrInTexts(searchInput, [
+        title,
+        description,
+        theme,
+      ]);
+
+      return { ...data, hidden: !isInputMatch };
+    });
+
+    setChannelData(searchedData);
+  }, [searchInput]);
+
+  const isSubstrInTexts = (substring: string, texts: string[]): boolean => {
+    const isSubstrInTexts = texts.reduce((sum, text) => {
+      return text.toLowerCase().includes(substring.toLowerCase())
+        ? sum + 1
+        : sum;
+    }, 0);
+    return isSubstrInTexts > 0;
+  };
+
   if (isLoading) {
     return <div>Loading resources...</div>;
   }
 
+  const handleSearchInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const searchInput = e.target.value.trim().toLowerCase();
+    setSearchInput(searchInput);
+  };
+
   return (
-    <div className={style.resources__wrapper}>
+    <div className={style.resources__wrapper} key="resources">
+      <Header children={<SearchBar onChange={handleSearchInput} />} />
+
       <ul className={style.resources__list}>
         {channelData.map((data) => (
-          <li key={data.id}>
+          <li key={data.id} className={data.hidden ? style.hidden : ""}>
             <Modal data={data} />
           </li>
         ))}
       </ul>
     </div>
   );
-});
-
-export default ResourcesPage;
+}
 
---
+++ src/utils/interfaces/ModalProps.ts
@@ -0,0 +1,8 @@
+import ChannelData from "./ChannelData";
+
+interface ModalProps extends ChannelData {
+  hidden?: boolean;
+}
+
+export default ModalProps;
+
