2fd0be95e14a9f034ea3aedd77b164707d183261
Sofya <s.cheboltasova@g.nsu.ru>
2024-07-20 06:59:49 +0700

    fix: add states and reducers

--- src/redux/slices/searchSlice.ts
+++ src/redux/slices/searchSlice.ts
@@ -1,15 +1,33 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { Coordinates } from "../../utils/interfaces/OpenTripMapApi/QueryCity";
+import SearchState from "../types";
+import { PlaceIdentifier } from "../../utils/interfaces/OpenTripMapApi/QueryPlace";
 
-const initialState: Coordinates = { lon: 37.6176, lat: 55.7558 };
+const initialState: SearchState = {
+  currentLocation: { lon: 37.6176, lat: 55.7558 },
+  allPlaces: [],
+  currentPlaces: [],
+  currentPage: 0,
+  itemsPerPage: 10,
+};
 
 const searchSlice = createSlice({
   name: "search",
   initialState,
   reducers: {
     setCity(state, action: PayloadAction<Coordinates>) {
-      state.lon = action.payload.lon;
-      state.lat = action.payload.lat;
+      state.currentLocation = action.payload;
+    },
+    setPlaces(state, action: PayloadAction<PlaceIdentifier[]>) {
+      state.allPlaces = action.payload;
+      state.currentPage = 0;
+      state.currentPlaces = state.allPlaces.slice(0, state.itemsPerPage);
+    },
+    setPage(state, action: PayloadAction<number>) {
+      state.currentPage = action.payload;
+      const start = state.currentPage * state.itemsPerPage;
+      const end = start + state.itemsPerPage;
+      state.currentPlaces = state.allPlaces.slice(start, end);
     },
   },
 });
--- src/redux/types.ts
+++ src/redux/types.ts
@@ -1,4 +1,14 @@
+import { Coordinates } from "../utils/interfaces/OpenTripMapApi/QueryCity";
+import { PlaceIdentifier } from "../utils/interfaces/OpenTripMapApi/QueryPlace";
 import store from "./store";
+
+export default interface SearchState {
+  currentLocation: Coordinates;
+  allPlaces: PlaceIdentifier[];
+  currentPlaces: PlaceIdentifier[];
+  currentPage: number;
+  itemsPerPage: number;
+}
 
 export type RootState = ReturnType<typeof store.getState>;
 
