c7ab1772ca4dfea40a8ebde29c26e7fa98b729fd
Sofya <s.cheboltasova@g.nsu.ru>
2024-07-23 04:23:25 +0700

    fix: remove unused states and reducers

--- src/redux/slices/mapSlice.ts
+++ src/redux/slices/mapSlice.ts
@@ -1,21 +1,13 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { BoundingBox, MapState } from "../types";
+import { MapState } from "../types";
 import { CityIdentifier } from "../../utils/interfaces/OpenTripMapApi/QueryCity";
 
 const centerLat = 55.7558;
 const centerLon = 37.6176;
-const radius = 250;
-
-const latChange = radius / 111000;
-const lonChange = radius / (111000 * Math.cos((centerLat * Math.PI) / 180));
 
 const initialState: MapState = {
   city: { lon: centerLon, lat: centerLat, name: "Москва" },
   zoom: 15,
-  boundingBox: {
-    southWest: [centerLon - lonChange, centerLat - latChange],
-    northEast: [centerLon + lonChange, centerLat + latChange],
-  },
 };
 
 const mapSlice = createSlice({
@@ -24,12 +16,6 @@ const mapSlice = createSlice({
   reducers: {
     setCity(state, action: PayloadAction<CityIdentifier>) {
       state.city = action.payload;
-    },
-    setBoundingBox(state, action: PayloadAction<BoundingBox>) {
-      state.boundingBox = action.payload;
-    },
-    setZoom(state, action: PayloadAction<number>) {
-      state.zoom = action.payload;
     },
   },
 });
